3e3aa1fbc8128c4837760ff014b81ef8ab4479f7
Guilherme Barros <barros.guilherme21@gmail.com>
2022-01-30 15:42:25 -0800

    Update solution.js

--- solution.js
+++ solution.js
@@ -2,14 +2,31 @@ const API_URL = 'https://randomuser.me/api?results=50'
 const result = document.getElementById('result')
 const filter = document.getElementById('filter')
 
+
+
+
+
+
 async function getData(){
     const response = await fetch(API_URL)
     const { results } = await response.json()
 
     //clear result
     result.innerHTML = '';
+    
+    let filterOp =
+    {
+        name: [],
+        location: []
+    };
+    buildFilter = (filterOp) =>
+    {
+        let query = {};
+        for (filter.textContent.constructor === results[])
+    }
+    console.log(filter.textContent) 
 
 }
 
 
-getData()
+getData();
