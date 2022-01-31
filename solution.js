651e180e79c364a72b45c23da0f14ca04a9e9206
Guilherme Barros <barros.guilherme21@gmail.com>
2022-01-31 09:41:25 -0800

    A few changes

--- solution.js
+++ solution.js
@@ -1,10 +1,18 @@
 const API_URL = 'https://randomuser.me/api?results=50'
 const result = document.getElementById('result')
 const filter = document.getElementById('filter')
+const listItems = []
 
-
-
-
+function filterData(searchTerm){
+    listItems.forEach(item => {
+        // console.log(item.innerText);
+        if(item.innerText.toLowerCase().includes(searchTerm.toLowerCase())){
+            item.classList.remove('hide')
+        }else{
+            item.classList.add('hide')
+        }
+    })
+}
 
 
 async function getData(){
@@ -12,21 +20,39 @@ async function getData(){
     const { results } = await response.json()
 
     //clear result
-    result.innerHTML = '';
-    
-    let filterOp =
-    {
-        name: [],
-        location: []
-    };
-    buildFilter = (filterOp) =>
-    {
-        let query = {};
-        for (filter.textContent.constructor === results[])
-    }
-    console.log(filter.textContent) 
+    result.innerHTML = ''
 
+    results.forEach((user) => {
+        console.log(user);
+
+        const li = document.createElement('li')
+
+        listItems.push(li)
+
+        const image = document.createElement('img')
+        image.src = `${user.picture.thumbnail}`
+        li.appendChild(image)
+
+        const container = document.createElement('div')
+        container.classList.add('user-info')
+        li.appendChild(container)
+
+        const username = document.createElement('h4')
+        username.innerText = `${user.name.first} ${user.name.last}`
+        container.appendChild(username)
+
+        const locationInfo = document.createElement('p')
+        locationInfo.innerText = `${user.location.city}, ${user.location.country}`
+        container.appendChild(locationInfo)
+
+        result.appendChild(li)
+
+    })
+    
 }
 
+getData()
 
-getData();
+filter.addEventListener('input', function(e){
+    filterData(e.target.value)
+})
